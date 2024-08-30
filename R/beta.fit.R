65f0c6aebc6c5e59b09a196c3adcb6c3f226ea69
Abu Saadat <103253429+SAADAT-Abu@users.noreply.github.com>
2024-08-30 12:40:53 +0200

    Update beta.fit.R
    Fixed library loading issue and NAN in MeanDiff

--- R/beta.fit.R
+++ R/beta.fit.R
@@ -14,9 +14,19 @@
 #' }
 #' @export
 beta.fit <- function(beta_matrix, sample_groups, chunk_size = 100, num_cores = 2) {
-
-  # Clean CpG IDs to remove numeric prefixes followed by a dot
-  rownames(beta_matrix) <- sub("^[0-9]+\\.", "", rownames(beta_matrix))
+  # Load necessary packages
+  if (!requireNamespace("betareg", quietly = TRUE)) {
+    install.packages("betareg")
+  }
+  if (!requireNamespace("parallel", quietly = TRUE)) {
+    install.packages("parallel")
+  }
+  if (!requireNamespace("tidyverse", quietly = TRUE)) {
+    install.packages("tidyverse")
+  }
+  library(betareg)
+  library(parallel)
+  library(tidyverse)
 
   # Get the number of CpG sites
   num_cpgs <- nrow(beta_matrix)
@@ -46,7 +56,6 @@ beta.fit <- function(beta_matrix, sample_groups, chunk_size = 100, num_cores = 2
       # Calculating MeanDiff
 
       mean_diff <- mean(beta[sample_groups == levels(sample_groups)[2]]) - mean(beta[sample_groups == levels(sample_groups)[1]])
-      
       # Store the results in a list
       chunk_results[[cpg]] <- c(
         Phi = phi,
@@ -78,9 +87,9 @@ beta.fit <- function(beta_matrix, sample_groups, chunk_size = 100, num_cores = 2
   # Apply multiple testing correction to phenotype p-values
   results_df$Phenotype_Pval_Adj <- p.adjust(results_df$Phenotype_Pval, method = "BH")
 
-  # Calculate logFC
+  # Calculate logFC as specified
   results_df$logFC <- with(results_df, ifelse(Phenotype_Coef >= 0, log2(Phenotype_Coef + 1), -log2(abs(Phenotype_Coef) + 1)))
+  # Clean CpG IDs to remove numeric prefixes followed by a dot
   rownames(results_df) <- sub("^[0-9]+\\.", "", rownames(results_df))
   return(results_df[,c(1:6,8,9,7)])
 }
-
